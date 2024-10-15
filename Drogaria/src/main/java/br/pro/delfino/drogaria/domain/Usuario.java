d91ea51c09d7990d8c9b957d0ef95aad5561c34d
monicaraquel <mraquel625@gmail.com>
2024-10-15 16:35:10 -0300

    implementação do cadastro usuarioBean, até a aula 216-Temas do PrimeFaces

--- Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
+++ Drogaria/src/main/java/br/pro/delfino/drogaria/domain/Usuario.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Transient;
 
 //vídeo 148
 
@@ -35,6 +36,21 @@ public class Usuario extends GenericDomain {
 	public Character getTipo() {
 		return tipo;
 	}
+	
+	@Transient
+	public String getTipoFormatado() {
+		String tipoFormatado = null;
+		
+		if(tipo == 'A') {
+			tipoFormatado = "Administrador";
+		} else if(tipo == 'B') {
+			tipoFormatado = "Balconista";
+		} else if(tipo == 'G') {
+			tipoFormatado = "Gerente";
+		}
+		
+		return tipoFormatado;
+	}
 
 	public void setTipo(Character tipo) {
 		this.tipo = tipo;
@@ -42,6 +58,16 @@ public class Usuario extends GenericDomain {
 
 	public Boolean getAtivo() {
 		return ativo;
+	}
+	
+	@Transient
+	public String getAtivoFormatado() {
+		String ativoFormatado = "Não";
+		
+		if(ativo) {
+			ativoFormatado = "Sim";
+		}
+		return ativoFormatado;
 	}
 
 	public void setAtivo(Boolean ativo) {
